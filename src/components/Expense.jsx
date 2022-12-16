9ea3a6fc5c1b209ab041f3b709e19dc4ec5f6c7a
Ruben Duarte <rdduarte1811@gmail.com>
2022-12-16 06:53:52 -0300

    commit

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { formatDate  } from './helpers';
+import IconExpenses from '../assets/img/icon-expenses.svg'
+import IconFood from '../assets/img/icon-food.svg'
+import IconFun from '../assets/img/icon-fun.svg'
+import IconHealt from '../assets/img/icon-healt.svg'
+import IconHouse from '../assets/img/icon-house.svg'
+import IconSaving from '../assets/img/icon-saving.svg'
+import IconSubs from '../assets/img/icon-subs.svg'
+
+
+const iconsDictionary = {
+  several : IconExpenses,
+  food : IconFood,
+  leisure : IconFun,
+  healt : IconHealt,
+  home : IconHouse,
+  saving : IconSaving,
+  subscriptions : IconSubs,
+}
 
 const Expense = ({ expense }) => {
    const { category, name, amount, id, date } = expense;
   return (
     <div className='expense shadow'>
       <div className='content-expense'>
-         {/* img */}
+         <img src={iconsDictionary[category]} alt="" />
          <div className="description-expense">
             <p className='category'> {category} </p>
             <p className='name-expense'> {name} </p>
